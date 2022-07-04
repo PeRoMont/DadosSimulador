f647b7cf4eb1f4488343ebd5b22cb5a95a94972e
Rodrigo <rodrigomonteiropereia54@gmail.com>
2022-07-04 18:36:43 -0300

    dados simulador

--- simuladordedados.py
+++ simuladordedados.py
@@ -1,6 +1,6 @@
 # Bibliotecas
-from random import randint
-from time import sleep
+from random import randint #Biblioteca para geração de números aleatoriamente
+from time import sleep #Biblioteca para o temporizados
 
 # Lista
 listanum = []
@@ -11,20 +11,19 @@ print('=' * 35)
 sleep(2)
 
 while True:
-    perg = str(input('Você quer rodar os dados? [S/N]: ')).strip().upper()
-    if perg == 'N':
+    perg = str(input('Você quer rodar os dados? [S/N]: ')).strip().upper() #Pergunta se quer rodar os dados
+    if perg == 'N': #Caso não queira rodar mais ele para
         break
     while perg != 'S':
-        perg = str(input(
-            'Escolha inválida! Insira novamente como [S ou N]\nInsira novamente: ')).strip().upper()
-        if perg == 'N':
+        perg = str(input('Escolha inválida! Insira novamente como [S ou N]\nInsira novamente: ')).strip().upper() #Pergunta novamente caso "S ou N" não tenha sido inserido
+        if perg == 'N': #Caso não queira rodar mais ele para
             break
     print('Girando os dados...')
-    dados = randint(1, 6)
+    dados = randint(1, 6) #Gerar o valor que vai sair no dado
     sleep(2)
     print(f'O número que saiu nos dados foi: {dados}')
     print('=' * 35)
-    listanum.append(dados)
+    listanum.append(dados) #Adicionar os valores a uma lista
 
-print(f'Lista com os números que saíram: {listanum}')
+print(f'Lista com os números que saíram: {listanum}') #Apresentar os valores da lista
 print('Programa Finalizado!\nATÉ LOGO!')
